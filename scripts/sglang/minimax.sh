2d1a1b81de80d1dd07ef8d6d9fee3151e95bcc50
Chen Chen <dashtotherock@gmail.com>
2026-04-21 10:01:44 -0600

    some fix

--- scripts/sglang/minimax.sh
+++ scripts/sglang/minimax.sh
@@ -4,7 +4,7 @@
 MODEL="/data/hf/saricles_MiniMax-M2.7-NVFP4-GB10"
 SERVED_MODEL_NAME="minimax"
 CONTEXT_LENGTH=65536
-MEM_FRACTION_STATIC=0.8
+MEM_FRACTION_STATIC=0.85
 TENSOR_PARALLEL=2
 HOST="0.0.0.0"
 PORT=30000
@@ -15,7 +15,7 @@ TOOL_CALL_PARSER="minimax-m2"
 python3 -m sglang.launch_server \
     --model-path ${MODEL} \
     --served-model-name ${SERVED_MODEL_NAME} \
-    --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 8}' \
+    --model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 4}' \
     --tp-size ${TENSOR_PARALLEL} \
     --host ${HOST} \
     --port ${PORT} \
@@ -25,6 +25,7 @@ python3 -m sglang.launch_server \
     --reasoning-parser minimax-append-think \
     --mem-fraction-static ${MEM_FRACTION_STATIC} \
     --max-total-tokens ${CONTEXT_LENGTH} \
-    --max-running-requests 3 \
+    --max-running-requests 2 \
     --kv-cache-dtype auto \
-    --quantization modelopt_fp4 
+    --quantization modelopt_fp4 \
+    --trust-remote-code 
