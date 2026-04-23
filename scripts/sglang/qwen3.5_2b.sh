1e127109dcc37943d4959b7b8d232872929d6199
Chen Chen <dashtotherock@gmail.com>
2026-04-22 21:40:08 -0600

    change back

--- scripts/sglang/qwen3.5_2b.sh
+++ scripts/sglang/qwen3.5_2b.sh
@@ -10,8 +10,9 @@ HOST="0.0.0.0"
 PORT=30000
 ATTENTION_BACKEND="triton"
 TOOL_CALL_PARSER="qwen3_coder"
+
 # Launch the server with single device
-SGLANG_DISABLE_MEM_CHECK=1 python3 -m sglang.launch_server \
+python3 -m sglang.launch_server \
     --model-path ${MODEL} \
     --served-model-name ${SERVED_MODEL_NAME} \
     --context-length ${CONTEXT_LENGTH} \
@@ -20,8 +21,7 @@ SGLANG_DISABLE_MEM_CHECK=1 python3 -m sglang.launch_server \
     --host ${HOST} \
     --port ${PORT} \
     --enable-metrics \
-    --attention-backend flashinfer \
-    --disable-cuda-graph \
+    --attention-backend ${ATTENTION_BACKEND} \
     --tool-call-parser ${TOOL_CALL_PARSER} \
     --reasoning-parser qwen3 \
     --trust-remote-code
